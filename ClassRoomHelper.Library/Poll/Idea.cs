e9d63504e0f58b6d84c1dbbc880aa1c378a5ff57
kxxt <renpengfei_8888@163.com>
2019-03-29 23:16:58 +0800

    Update

---
+++ ClassRoomHelper.Library/Poll/Idea.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomHelper.Library.Poll
+{
+	public class Idea
+	{
+		public bool UpVote(string student)
+		{
+			if (StudentInfo.ContainsKey(student))
+			{
+				if (StudentInfo[student])
+				{
+					return false;
+				}
+				else
+				{
+					StudentInfo[student] = true;
+					Votes++;
+					return true;
+				}
+			}
+			StudentInfo.Add(student, true);
+			Votes++;
+			return true;
+		}
+		public bool DownVote(string student)
+		{
+			if (StudentInfo.ContainsKey(student))
+			{
+				if (StudentInfo[student])
+				{
+					StudentInfo[student] = false;
+					Votes--;
+					return true;
+				}
+				else
+				{
+					
+					return false;
+				}
+			}
+			return false;
+		}
+		Dictionary<string, bool> StudentInfo;
+		string Desp;
+		int Votes;
+		public Idea()
+		{
+			Desp = "未命名";
+			Votes = 0;
+			StudentInfo = new Dictionary<string, bool>();
+		}
+	}
+}
--- ClassRoomHelper/Windows/Vote.Designer.cs
+++ ClassRoomHelper/Windows/Vote.Designer.cs
@@ -28,37 +28,128 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.sfScrollFrame1 = new Syncfusion.WinForms.Controls.SfScrollFrame();
-			this.panel1 = new System.Windows.Forms.Panel();
+			this.modernButton1 = new RsWork.UI.Controls.ModernButton();
+			this.titleLabel1 = new ClassRoomHelper.Windows.Controls.TitleLabel();
+			this.modernButton2 = new RsWork.UI.Controls.ModernButton();
+			this.modernButton3 = new RsWork.UI.Controls.ModernButton();
+			this.modernButton4 = new RsWork.UI.Controls.ModernButton();
 			this.SuspendLayout();
 			// 
-			// sfScrollFrame1
+			// modernButton1
 			// 
-			this.sfScrollFrame1.Control = this.panel1;
+			this.modernButton1.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton1.BorderColor = System.Drawing.Color.White;
+			this.modernButton1.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton1.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton1.FlatAppearance.BorderSize = 2;
+			this.modernButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton1.Font = new System.Drawing.Font("黑体", 15F);
+			this.modernButton1.ForeColor = System.Drawing.Color.White;
+			this.modernButton1.Location = new System.Drawing.Point(26, 82);
+			this.modernButton1.Name = "modernButton1";
+			this.modernButton1.Size = new System.Drawing.Size(159, 319);
+			this.modernButton1.TabIndex = 0;
+			this.modernButton1.Text = "单选投票\r\n\r\n单选题";
+			this.modernButton1.UseVisualStyleBackColor = false;
 			// 
-			// panel1
+			// titleLabel1
 			// 
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel1.Location = new System.Drawing.Point(0, 0);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(866, 413);
-			this.panel1.TabIndex = 0;
+			this.titleLabel1.AutoSize = true;
+			this.titleLabel1.BackColor = System.Drawing.Color.Transparent;
+			this.titleLabel1.Font = new System.Drawing.Font("黑体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.titleLabel1.ForeColor = System.Drawing.Color.White;
+			this.titleLabel1.Location = new System.Drawing.Point(12, 9);
+			this.titleLabel1.Name = "titleLabel1";
+			this.titleLabel1.Size = new System.Drawing.Size(356, 48);
+			this.titleLabel1.TabIndex = 1;
+			this.titleLabel1.Text = "请选择投票类型";
+			// 
+			// modernButton2
+			// 
+			this.modernButton2.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton2.BorderColor = System.Drawing.Color.White;
+			this.modernButton2.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton2.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton2.FlatAppearance.BorderSize = 2;
+			this.modernButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton2.Font = new System.Drawing.Font("黑体", 15F);
+			this.modernButton2.ForeColor = System.Drawing.Color.White;
+			this.modernButton2.Location = new System.Drawing.Point(374, 82);
+			this.modernButton2.Name = "modernButton2";
+			this.modernButton2.Size = new System.Drawing.Size(159, 319);
+			this.modernButton2.TabIndex = 2;
+			this.modernButton2.Text = "简单选人\r\n\r\n从班级的所有学生中经投票为一项活动选出几名候选人";
+			this.modernButton2.UseVisualStyleBackColor = false;
+			// 
+			// modernButton3
+			// 
+			this.modernButton3.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton3.BorderColor = System.Drawing.Color.White;
+			this.modernButton3.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton3.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton3.FlatAppearance.BorderSize = 2;
+			this.modernButton3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton3.Font = new System.Drawing.Font("黑体", 15F);
+			this.modernButton3.ForeColor = System.Drawing.Color.White;
+			this.modernButton3.Location = new System.Drawing.Point(548, 82);
+			this.modernButton3.Name = "modernButton3";
+			this.modernButton3.Size = new System.Drawing.Size(159, 319);
+			this.modernButton3.TabIndex = 3;
+			this.modernButton3.Text = "复杂选人\r\n\r\n从班级的所有学生中经投票为多项项活动选出几名候选人";
+			this.modernButton3.UseVisualStyleBackColor = false;
+			// 
+			// modernButton4
+			// 
+			this.modernButton4.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton4.BorderColor = System.Drawing.Color.White;
+			this.modernButton4.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton4.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton4.FlatAppearance.BorderSize = 2;
+			this.modernButton4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton4.Font = new System.Drawing.Font("黑体", 15F);
+			this.modernButton4.ForeColor = System.Drawing.Color.White;
+			this.modernButton4.Location = new System.Drawing.Point(200, 82);
+			this.modernButton4.Name = "modernButton4";
+			this.modernButton4.Size = new System.Drawing.Size(159, 319);
+			this.modernButton4.TabIndex = 4;
+			this.modernButton4.Text = "多选投票\r\n\r\n多选题";
+			this.modernButton4.UseVisualStyleBackColor = false;
 			// 
 			// Vote
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(866, 413);
-			this.Controls.Add(this.panel1);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.DodgerBlue;
+			this.ClientSize = new System.Drawing.Size(731, 423);
+			this.Controls.Add(this.modernButton4);
+			this.Controls.Add(this.modernButton3);
+			this.Controls.Add(this.modernButton2);
+			this.Controls.Add(this.titleLabel1);
+			this.Controls.Add(this.modernButton1);
 			this.Name = "Vote";
 			this.Text = "班级投票";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private Syncfusion.WinForms.Controls.SfScrollFrame sfScrollFrame1;
-		private System.Windows.Forms.Panel panel1;
+		private RsWork.UI.Controls.ModernButton modernButton1;
+		private Controls.TitleLabel titleLabel1;
+		private RsWork.UI.Controls.ModernButton modernButton2;
+		private RsWork.UI.Controls.ModernButton modernButton3;
+		private RsWork.UI.Controls.ModernButton modernButton4;
 	}
 }
